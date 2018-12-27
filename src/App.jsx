be3a63e1d38dc5b2a809572a8d44c8312e80b71e
alfredlam42 <alfred.lam.42@gmail.com>
2018-12-27 15:52:28 -0800

    refactor routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo'
 
 import Container from 'components/common/Container'
-import Header from 'components/common/Header'
 
 import AccountsPageContainer from 'containers/AccountsPage'
+import HeaderContainer from 'containers/common/Header'
 
 import { BASE_PATH, ACCOUNTS_PATH } from 'constants/urls'
 
@@ -15,21 +15,17 @@ import './app.module.scss'
 
 const App = () => (
     <ApolloProvider client={getApolloClient()}>
-        <Container className={'h-100'}>
-            <BrowserRouter>
-                <React.Fragment>
-                    <Header />
+        <BrowserRouter>
+            <Container className={'h-100'}>
+                <HeaderContainer />
 
-                    <Switch>
-                        <Redirect exact from={BASE_PATH} to={ACCOUNTS_PATH} />
-
-                        <Route exact to={ACCOUNTS_PATH} component={AccountsPageContainer} />
-
-                        <Redirect from={'*'} to={'/'} />
-                    </Switch>
-                </React.Fragment>
-            </BrowserRouter>
-        </Container>
+                <Switch>
+                    <Redirect exact from={BASE_PATH} to={ACCOUNTS_PATH} />
+                    <Route exact path={ACCOUNTS_PATH} component={AccountsPageContainer} />
+                    <Route component={() => (<div>not found</div>)} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     </ApolloProvider>
 )
 
