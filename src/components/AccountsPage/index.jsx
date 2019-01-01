47d3625ac857edf8bb8779c1c1c573f54a62cdf7
alfredlam42 <alfred.lam.42@gmail.com>
2018-12-31 16:35:24 -0800

    update accounts page

--- src/components/AccountsPage/index.jsx
+++ src/components/AccountsPage/index.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import cx from 'classnames'
 
 import AccountFromQueryType from 'types/accountFromQuery'
 
+import { formatCurrency } from 'utils/numbers'
+
+import styles from './accounts-page.module.scss'
+
 const AccountsPage = ({ accounts }) => (
-    <div className={'ph3 pv4'}>
-        {accounts && accounts.map(({ id, name }) => (
-            <div key={id}>{name}</div>
-        ))}
+    <div className={'ph3 pv4 fitScreen'}>
+        <div className={styles.container}>
+            {accounts && accounts.map(({ id, name, balance }) => (
+                <Link
+                    key={id}
+                    to={`/accounts/${id}`}
+                    className={cx(styles.accountItem, 'flex items-center justify-between pv3 ph3')}>
+                    <div>{name}</div>
+                    <div>{formatCurrency(balance)}</div>
+                </Link>
+            ))}
+        </div>
     </div>
 )
 
